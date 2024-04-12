f57e02cee2a9b636f2077c36728849a5f07160c6
Joker0602 <147969177+Joker0602@users.noreply.github.com>
2024-04-12 17:31:40 +0530

    changes in student

--- StudentWeb/StudentWeb/src/app/app.module.ts
+++ StudentWeb/StudentWeb/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { StudentFormComponent } from './forms/student-form/student-form.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { StudentService } from '../services/student.service';
+import { StudentService } from './services/student.service';
 
 @NgModule({
   declarations: [
--- StudentWeb/StudentWeb/src/app/forms/student-form/student-form.component.ts
+++ StudentWeb/StudentWeb/src/app/forms/student-form/student-form.component.ts
@@ -1,155 +1,184 @@
 import { Component } from '@angular/core';
-import { StudentService } from '../../../services/student.service';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
-import { studentMaster } from '../../../models/StudentMaster';
+import { studentMaster } from 'src/app/models/StudentMaster';
+import { StudentService } from 'src/app/services/student.service';
 
 @Component({
   selector: 'app-student-form',
   templateUrl: './student-form.component.html',
-  styleUrls: ['./student-form.component.css']
+  styleUrls: ['./student-form.component.css'],
 })
 export class StudentFormComponent {
-  studentList: studentMaster[]=[]
+  studentList: studentMaster[] = [];
   studentForm: FormGroup | any;
-  student: studentMaster = {} as studentMaster
+  student: studentMaster = {} as studentMaster;
   MxId: number = 0;
-  mobilePattern = '/^\d+$/'
-  saveBtn: boolean = false;
+  mobilePattern = '/^d+$/';
+
+  saveBtn: boolean = true;
   updateBtn: boolean = false;
   deleteBtn: boolean = false;
+
   constructor(
     private studentService: StudentService,
     private formBuilder: FormBuilder
-  ) { }
+  ) {}
 
   ngOnInit() {
 
     this.getStudent();
     this.maxId();
 
-    this.saveBtn = false;
-    this.updateBtn = true;
-    this.deleteBtn = true;
+
   }
 
   //get student all
+
   getStudent() {
+
     this.studentService.getStudentAll().subscribe((res) => {
-
       this.studentList = res;
-
     });
 
     this.maxId();
   }
 
+  //MaxId
   maxId() {
+
     this.studentService.maxId().subscribe((res) => {
-      
       if (res === 0) {
         this.MxId = 1;
       } else {
         this.MxId = res;
         this.student.sId = res + 1;
       }
-    })
+    });
+
   }
 
   //Save
+
   Save(studentForm: NgForm) {
-    if (this.student.sName != '' && this.student.sEmail != '' && studentForm.valid) {
-      this.student.sName = this.student.sName;
-      this.student.sEmail = this.student.sEmail;
-      this.student.age = this.student.age;
-      this.student.contact = this.student.contact;
+    if (
+      this.student.sName != '' &&
+      this.student.sEmail != '' &&
+      studentForm.valid
+    ) {
+      // this.student.sName = this.student.sName;
+      // this.student.sEmail = this.student.sEmail;
+      // this.student.age = this.student.age;
+      // this.student.contact = this.student.contact;
 
       //passing data to service
-      this.studentService.addStudent(this.student).subscribe((res) => {
 
+      this.studentService.addStudent(this.student).subscribe((res) => {
         this.clear();
         this.getStudent();
-        this.maxId()
+        this.maxId();
 
-        alert("Saved !")
+        alert('Saved !');
 
-        this.saveBtn = false;
-        this.updateBtn = true;
-        this.deleteBtn = true;
+        this.saveBtn = true;
+        this.updateBtn = false;
+        this.deleteBtn = false;
+      },error=>{
+        alert("error while saving");
       });
+
+
     } else {
-      alert("Invalid Field");
+      alert('Invalid Field');
     }
   }
 
-
   //update
+
   update(studentForm: NgForm) {
-    if (this.student.sName != " " && this.student.sEmail != " ") {
-      this.student.sId = this.student.sId;
-      this.student.sName = this.student.sName;
-      this.student.sEmail = this.student.sEmail;
-      this.student.age = this.student.age;
-      this.student.contact = this.student.contact;
+    if (this.student.sName != ' ' && this.student.sEmail != ' ') {
+
+      // this.student.sName = this.student.sName;
+      // this.student.sEmail = this.student.sEmail;
+      // this.student.age = this.student.age;
+      // this.student.contact = this.student.contact;
       console.log(this.student);
+
       //passing data to service
       this.studentService.updateStudent(this.student).subscribe((res) => {
         this.clear();
         this.getStudent();
-        this.maxId()
+        this.maxId();
 
-        alert("Updated !");
+        alert('Updated !');
 
-        this.saveBtn = false;
-        this.updateBtn = true;
-        this.deleteBtn = true;
+        this.saveBtn = true;
+        this.updateBtn = false;
+        this.deleteBtn = false;
+      },error=>{
+        alert("error while updating")
       });
+
+
     } else {
-      alert("Invalid Field");
+      alert('Invalid Field');
     }
   }
 
   //get student by id
+
   getById(id: number) {
+
     this.studentService.getById(id).subscribe((res) => {
+
       this.student.sId = res.sId;
       this.student.sEmail = res.sEmail;
       this.student.sName = res.sName;
       this.student.age = res.age;
       this.student.contact = res.contact;
 
+      this.saveBtn = false;
+      this.updateBtn = true;
+      this.deleteBtn = true;
+
+    },error=>{
+      alert("error while getting data")
     });
 
-    this.saveBtn = true
-    this.updateBtn = false;
-    this.deleteBtn=false
+
   }
 
   //Clear Input
+
   clear() {
     this.student.age = 0;
     this.student.sName = '';
     this.student.contact = '';
     this.student.sEmail = '';
-   
+
     this.maxId();
 
-    this.saveBtn = false;
-    this.updateBtn = true;
-    this.deleteBtn = true;
+    this.saveBtn = true;
+    this.updateBtn = false;
+    this.deleteBtn = false;
   }
 
-  //delete 
+  //delete
+
   Delete() {
     if (this.student.sId != 0) {
       const Id = this.student.sId;
-      this.studentService.deleteStudent(Id).subscribe((res) => {
 
-        alert("Successfully Deleted");
+
+      this.studentService.deleteStudent(Id).subscribe((res) => {
+        alert('Successfully Deleted');
 
         this.clear();
         this.getStudent();
         this.maxId();
-      })
+      },error=>{
+        alert('Error while Delete')
+      });
+
     }
   }
 }
